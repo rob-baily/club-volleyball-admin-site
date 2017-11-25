e6276394b2f2f1fc8ce0a9d9241b4ddeb70037e4
Rob Baily <rbaily@etranssystems.com>
2017-11-25 06:29:58 -0500

    Complete adding team name to entry filter.

--- web-application/src/main/java/org/brycvolleyball/admin/repositories/TournamentEntryRepository.java
+++ web-application/src/main/java/org/brycvolleyball/admin/repositories/TournamentEntryRepository.java
@@ -8,6 +8,10 @@ import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface TournamentEntryRepository extends MongoRepository<TournamentEntry,String> {
-	@Query("{ $or : [ { $or :  [ { $where: '\"?0\" == \" null \"' }, { $where: '\"?0\" == \"\"' } ] }, { status : '?0' } ] }")
-	public Page<TournamentEntry> findTournamentEntries(@Param("status") String status, Pageable pageable);
+	@Query("{ $and : [ " +
+			"{ $or : [ { $or :  [ { $where: '\"?0\" == \" null \"' }, { $where: '\"?0\" == \"\"' } ] }, { status : '?0' } ] }" +
+			", { $or : [ { $or :  [ { $where: '\"?1\" == \" null \"' }, { $where: '\"?1\" == \"\"' } ] }, { teamName : '?1' } ] }" +
+			"] } ")
+	public Page<TournamentEntry> findTournamentEntries(
+			@Param("status") String status, @Param("teamName") String teamName, Pageable pageable);
 }
