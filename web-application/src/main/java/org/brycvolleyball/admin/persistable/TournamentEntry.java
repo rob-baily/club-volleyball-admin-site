facd84c2e81be5523a7ef5d6925c4d27f986a489
Rob Baily <rbaily@etranssystems.com>
2018-01-02 18:35:42 -0500

    Add Denied as tournament entry status.

--- web-application/src/main/java/org/brycvolleyball/admin/persistable/TournamentEntry.java
+++ web-application/src/main/java/org/brycvolleyball/admin/persistable/TournamentEntry.java
@@ -12,7 +12,8 @@ public class TournamentEntry {
 		Paid,
 		Accepted,
 		Withdrawn,
-		Cancelled
+		Cancelled,
+		Denied
 	}
 
 	@Id
--- web-application/src/main/resources/static/javascript/admin-angular/controllers.js
+++ web-application/src/main/resources/static/javascript/admin-angular/controllers.js
@@ -12,7 +12,8 @@ function StatusListHelper() {
             "Paid",
             "Accepted",
             "Withdrawn",
-            "Cancelled"
+            "Cancelled",
+            "Denied"
         ];
     }
 }
